7a6758b715c2e72ec972d61e0eb13f3133eb98df
Adrian Webb <adrian.webb@gsa.gov>
2016-05-22 16:17:07 -0400

    Adding an initial composer-rebuild script.

---
+++ scripts/composer-rebuild.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e
+
+#-------------------------------------------------------------------------------
+# Default properties
+
+SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
+TOP_DIR="$SCRIPT_DIR/.."
+
+#-------------------------------------------------------------------------------
+# Property initialization
+
+while [[ $# > 0 ]]
+do
+  key="$1"
+
+  case $key in
+    -h|--help)
+      message="
+ Usage: ./composer-rebuild.sh [ -h ]
+ 
+   -h | --help  |  Display this help message
+"
+      echo "$message"
+      exit 0
+    ;;
+    *)
+      # unknown option
+    ;;
+  esac
+  shift
+done
+
+#-------------------------------------------------------------------------------
+# Begin
+
+# Build a fresh application code base
+cd "$TOP_DIR"
+
+rm -f "composer.lock"
+rm -Rf "vendor"
+rm -Rf "htdocs"
+composer install
+
+echo "Successfully rebuilt Drupal application"
