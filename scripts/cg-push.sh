b7408feb6be4646cc452ce6de222e4cf78d3e548
Adrian Webb <adrian.webb@gsa.gov>
2016-05-22 15:39:16 -0400

    Adding an initial cg-push script.

---
+++ scripts/cg-push.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+set -e
+
+#-------------------------------------------------------------------------------
+# Default properties
+
+SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
+TOP_DIR="$SCRIPT_DIR/.."
+
+APP_DEFAULT_NAME="project-planner"
+APP_NAME="$APP_DEFAULT_NAME"
+
+APP_DEFAULT_START="start"
+APP_START="$APP_DEFAULT_START"
+
+#-------------------------------------------------------------------------------
+# Property initialization
+
+while [[ $# > 0 ]]
+do
+  key="$1"
+
+  case $key in
+    -n|--name)
+      APP_NAME="$2"
+      shift
+    ;;
+    -o|--no-start)
+      APP_START="stop"
+      shift
+    ;;
+    -h|--help)
+      message="
+ Usage: ./cg-deploy.sh [ -h ]
+ 
+   -h | --help       |  Display this help message
+   -o | --no-start   |  Create the application and related services but do not start (default: $APP_START)
+   -n | --name       |  Specify the name of the drupal application (default: $APP_NAME)
+"
+      echo "$message"
+      exit 0
+    ;;
+    *)
+      # unknown option
+    ;;
+  esac
+  shift
+done
+
+#---
+
+if [ "$APP_NAME" != "$APP_DEFAULT_NAME" ]; then
+  echo "Drupal application name: $APP_NAME"
+else
+  echo "Drupal application name (override with -n|--name): $APP_NAME"
+fi
+
+if [ "$APP_START" != "$APP_DEFAULT_START" ]; then
+  echo "Drupal application state: $APP_START"
+else
+  echo "Drupal application state (override with -o|--no-start): $APP_START"
+fi
+
+#-------------------------------------------------------------------------------
+# Begin
+
+# Prepare application (build a fresh copy)
+cd "$TOP_DIR"
+
+rm -f "composer.lock"
+rm -Rf "vendor"
+rm -Rf "htdocs"
+composer install # Rebuild the code base to ensure we have a clean remote copy
+
+# Push application to cloud.gov 
+# (but do not start it until we create and attach the services)
+cf push --no-start
+
+if [ "$APP_START" == "start" ]; then
+  # Start application
+  cf start "$APP_NAME"
+fi
+
+echo "Successfully pushed $APP_NAME application"
