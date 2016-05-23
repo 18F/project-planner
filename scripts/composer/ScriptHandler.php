6128367ec4dc4db63b813580d46adcdc8c8335aa
Adrian Webb <adrian.webb@gsa.gov>
2016-05-22 21:29:33 -0400

    Stripping down the script handler composer functionality for now.

--- scripts/composer/ScriptHandler.php
+++ scripts/composer/ScriptHandler.php
@@ -28,25 +28,15 @@ class ScriptHandler {
     $root = static::getDrupalRoot(getcwd());
 
     // Prepare the settings file for installation
-    if (!$fs->exists($root . '/sites/default/settings.php')) {
-      $fs->copy($root . '/sites/default/default.settings.php', $root . '/sites/default/settings.php');
-      $fs->chmod($root . '/sites/default/settings.php', 0666);
-      $event->getIO()->write("Create a sites/default/settings.php file with chmod 0666");
+    if ($fs->exists($root . '/sites/default/settings.php')) {
+      $fs->chmod($root . '/sites/default/settings.php', 0444);
+      $event->getIO()->write("Ensure sites/default/settings.php file is read only");
     }
 
     // Prepare the services file for installation
-    if (!$fs->exists($root . '/sites/default/services.yml')) {
-      $fs->copy($root . '/sites/default/default.services.yml', $root . '/sites/default/services.yml');
-      $fs->chmod($root . '/sites/default/services.yml', 0666);
-      $event->getIO()->write("Create a sites/default/services.yml file with chmod 0666");
-    }
-
-    // Create the files directory with chmod 0777
-    if (!$fs->exists($root . '/sites/default/files')) {
-      $oldmask = umask(0);
-      $fs->mkdir($root . '/sites/default/files', 0777);
-      umask($oldmask);
-      $event->getIO()->write("Create a sites/default/files directory with chmod 0777");
+    if ($fs->exists($root . '/sites/default/services.yml')) {
+      $fs->chmod($root . '/sites/default/services.yml', 0444);
+      $event->getIO()->write("Ensure sites/default/services.yml file is read only");
     }
   }
 }
