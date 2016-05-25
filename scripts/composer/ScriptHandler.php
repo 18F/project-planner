a82cc3d5d6bb36d7dad3c4c0ee159c0cd57606f0
Adrian Webb <adrian.webb@gsa.gov>
2016-05-25 10:12:06 -0400

    Readding files directory permissions modification in the ScriptHandler composer script.

--- scripts/composer/ScriptHandler.php
+++ scripts/composer/ScriptHandler.php
@@ -38,5 +38,13 @@ class ScriptHandler {
       $fs->chmod($root . '/sites/default/services.yml', 0444);
       $event->getIO()->write("Ensure sites/default/services.yml file is read only");
     }
+
+    // Create the files directory with chmod 0777
+    if (!$fs->exists($root . '/sites/default/files')) {
+      $oldmask = umask(0);
+      $fs->mkdir($root . '/sites/default/files', 0777);
+      umask($oldmask);
+      $event->getIO()->write("Create a sites/default/files directory with chmod 0777");
+    }
   }
 }
