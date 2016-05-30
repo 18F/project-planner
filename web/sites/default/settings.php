5a7563882c71e6ae7247f9d1be0039a5ae5814b2
Adrian Webb <adrian.webb@gsa.gov>
2016-05-30 18:10:04 -0400

    Adding a trusted host pattern in the settings php for apps.cloud.gov.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -794,6 +794,9 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
  * will allow the site to run off of all variants of example.com and
  * example.org, with all subdomains included.
  */
+$settings['trusted_host_patterns'] = array(
+ '^.+\.apps.cloud\.gov$',
+);
 
 /**
  * Load local development override configuration, if available.
