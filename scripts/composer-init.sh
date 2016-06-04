ca02d20a9ac0e88f4a002da31d352240f635532b
Adrian Webb <adrian.webb@gsa.gov>
2016-06-04 03:21:50 -0400

    Splitting the initialization of composer out into it's own composer-init script (from drush-init).

---
+++ scripts/composer-init.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+set -e
+
+#-------------------------------------------------------------------------------
+# Default properties
+
+DEFAULT_TEMP_DIR="/tmp"
+TEMP_DIR="$DEFAULT_TEMP_DIR"
+
+DEFAULT_BIN_DIR="$HOME/bin"
+BIN_DIR="$DEFAULT_BIN_DIR"
+
+#-------------------------------------------------------------------------------
+# Property initialization
+
+while [[ $# > 0 ]]
+do
+  key="$1"
+
+  case $key in
+    -d|--dir)
+      TEMP_DIR="$2"
+      shift
+    ;;
+    -b|--bin)
+      BIN_DIR="$2"
+      shift
+    ;;
+    -h|--help)
+      message="
+Usage: ./composer-init.sh [ -h ] [ --dir {temp directory} ] [ --bin {bin directory} ]
+ 
+   -h | --help      |  Display this help message
+   -d | --dir       |  Specify the temp directory in which Composer will be downloaded. (default: $TEMP_DIR)  
+                       -> NOTE: This directory must already exist.
+   -b | --bin       |  Specify the bin directory where the executable Composer will live. (default: $BIN_DIR)  
+                       -> NOTE: This directory will be created if it does not already exist.
+"
+      echo "$message"
+      exit 0
+    ;;
+    *)
+      # unknown option
+    ;;
+  esac
+  shift
+done
+
+#---
+
+if [ "$TEMP_DIR" != "$DEFAULT_TEMP_DIR" ]; then
+  echo "Temp directory: $TEMP_DIR"
+else
+  echo "Temp directory (override with -d|--dir): $TEMP_DIR"
+fi
+
+if [ "$BIN_DIR" != "$DEFAULT_BIN_DIR" ]; then
+  echo "Bin directory: $BIN_DIR"
+else
+  echo "Bin directory (override with -b|--bin): $BIN_DIR"
+fi
+
+
+#-------------------------------------------------------------------------------
+# Begin
+
+# Head over to the tmp directory
+cd "$TEMP_DIR"
+
+# Ensure composer is installed
+curl -sS https://getcomposer.org/installer | php
+
+mkdir -p "$BIN_DIR"
+mv composer.phar "$BIN_DIR/composer"
+
+echo "Successfully installed Composer"
--- scripts/drush-init.sh
+++ scripts/drush-init.sh
@@ -5,11 +5,17 @@ set -e
 #-------------------------------------------------------------------------------
 # Default properties
 
+SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
+TOP_DIR="$SCRIPT_DIR/.."
+
 DRUSH_DEFAULT_DIR="$HOME/.drush"
 DRUSH_DIR="$DRUSH_DEFAULT_DIR"
 
 DRUSH_DEFAULT_BRANCH="8.1.2"
 DRUSH_BRANCH="$DRUSH_DEFAULT_BRANCH"
+
+DEFAULT_BIN_DIR="$HOME/bin"
+BIN_DIR="$DEFAULT_BIN_DIR"
 
 #-------------------------------------------------------------------------------
 # Property initialization
@@ -23,8 +29,12 @@ do
       DRUSH_DIR="$2"
       shift
     ;;
-    -b|--branch)
+    -r|--branch)
       DRUSH_BRANCH="$2"
+      shift
+    ;;
+    -b|--bin)
+      BIN_DIR="$2"
       shift
     ;;
     -h|--help)
@@ -34,7 +44,9 @@ do
    -h | --help      |  Display this help message
    -d | --dir       |  Specify the directory in which Drush will be downloaded. (default: $DRUSH_DIR)  
                        -> NOTE: This directory will be deleted if it exists then recreated.
-   -b | --branch    |  Specify the Git Drush branch to install from (default: $DRUSH_BRANCH)
+   -r | --branch    |  Specify the Git Drush branch to install from (default: $DRUSH_BRANCH)
+   -b | --bin       |  Specify the bin directory where the executable Composer will live. (default: $BIN_DIR)  
+                       -> NOTE: This directory will be created if it does not already exist.
 "
       echo "$message"
       exit 0
@@ -57,7 +69,13 @@ fi
 if [ "$DRUSH_BRANCH" != "$DRUSH_DEFAULT_BRANCH" ]; then
   echo "Drush branch: $DRUSH_BRANCH"
 else
-  echo "Drush branch (override with -b|--branch): $DRUSH_BRANCH"
+  echo "Drush branch (override with -r|--branch): $DRUSH_BRANCH"
+fi
+
+if [ "$BIN_DIR" != "$DEFAULT_BIN_DIR" ]; then
+  echo "Bin directory: $BIN_DIR"
+else
+  echo "Bin directory (override with -b|--bin): $BIN_DIR"
 fi
 
 #-------------------------------------------------------------------------------
@@ -69,18 +87,13 @@ git clone -b "$DRUSH_BRANCH" https://github.com/drush-ops/drush.git "$DRUSH_DIR"
 cd "$DRUSH_DIR"
 
 # Ensure composer is installed
-curl -sS https://getcomposer.org/installer | php
-
-mkdir -p "$HOME/bin"
-export PATH="$HOME/bin:$PATH"
-
-mv composer.phar "$HOME/bin/composer"
+"$SCRIPT_DIR/composer-init.sh" -d "$DRUSH_DIR" -b "$BIN_DIR"
 
 # Run composer install
 composer install
 
 # Link drush executable to path
-rm -f "$HOME/bin/drush"
-ln -s "$HOME/.drush/drush" "$HOME/bin/drush"
+rm -f "$BIN_DIR/drush"
+ln -s "$DRUSH_DIR/drush" "$BIN_DIR/drush"
 
 echo "Successfully installed Drush"
